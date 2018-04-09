bcb7dcd72ef8881d7ee3f782493773ad7e694e9a
Taisuke Yamada <tai@rakugaki.org>
2018-04-09 16:40:01 +0900

    Initial working version

---
+++ app/src/main/java/com/example/tai/kotlintest/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.tai.kotlintest
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.textView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun toastMe(view: View) {
+        // val myToast = Toast.makeText(this, message, duration);
+        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
+        myToast.show()
+    }
+
+    fun countMe(view: View) {
+        // old way without following:
+        // import kotlinx.android.synthetic.main.activity_main.textView
+        if (false) {
+            // Get the text view
+            val showCountTextView = findViewById(R.id.textView) as TextView
+
+            // Get the value of the text view.
+            val countString = showCountTextView.text.toString()
+
+            // Convert value to a number and increment it
+            var count: Int = Integer.parseInt(countString)
+            count++
+
+            showCountTextView.text = count.toString()
+        }
+
+        val countString = textView.text.toString()
+
+        // Convert value to a number and increment it
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        // Display the new value in the text view.
+        textView.text = count.toString();
+    }
+
+    fun randomMe(view: View) {
+        // Create an Intent to start the second activity
+        val randomIntent = Intent(this, SecondActivity::class.java)
+
+        // Get the current value of the text view.
+        val countString = textView.text.toString()
+
+        // Convert the count to an int
+        val count = Integer.parseInt(countString)
+
+        // Add the count to the extras for the Intent.
+        randomIntent.putExtra(SecondActivity.TOTAL_COUNT, count)
+
+        // Start the new activity.
+        startActivity(randomIntent)
+    }
+}
---
+++ app/src/main/java/com/example/tai/kotlintest/SecondActivity.kt
@@ -0,0 +1,45 @@
+package com.example.tai.kotlintest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+
+import kotlinx.android.synthetic.main.activity_second.textview_label
+import kotlinx.android.synthetic.main.activity_second.textview_random
+
+
+class SecondActivity : AppCompatActivity() {
+
+    companion object {
+        const val TOTAL_COUNT = "total_count"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        // update view using value in intent
+        showRandomNumber()
+    }
+
+    fun showRandomNumber() {
+        // Get the count from the intent extras
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
+
+        // Generate the random number
+        val random = Random()
+        var randomInt = 0
+        // Add one because the bound is exclusive
+        if (count > 0) {
+            // Add one because the bound is exclusive
+            randomInt = random.nextInt(count + 1)
+        }
+
+        // Display the random number.
+        textview_random.text = Integer.toString(randomInt)
+
+        // Substitute the max value into the string resource
+        // for the heading, and update the heading
+        textview_label.text = getString(R.string.random_heading, count)
+    }
+}
